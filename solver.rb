e33d1e34b4bad2182a4518a3d84cc32d575a8c1a
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-07-06 14:42:44 +0200

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -1,19 +1,30 @@
 class Solver
-    def factorial(n)
-      raise ArgumentError, "Factorial is defined only for non-negative integers." if n.negative?
-  
-      return 1 if n.zero?
-  
-      result = 1
-      (1..n).each do |i|
-        result *= i
-      end
-      result
+  def factorial(n)
+    raise ArgumentError, "Factorial is defined only for non-negative integers." if n.negative?
+
+    return 1 if n.zero?
+
+    result = 1
+    (1..n).each do |i|
+      result *= i
     end
-  
-    def reverse(word)
-      word.reverse
-    end
+    result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
   #fizzbuzz
-    
+  def fizzbuzz(n)
+    if (n % 3).zero? && (n % 5).zero?
+      "fizzbuzz"
+    elsif (n % 3).zero?
+      "fizz"
+    elsif (n % 5).zero?
+      "buzz"
+    else
+      n.to_s
+    end
+  end
+end
   
