25dbfedbd6630dc002652f0658683d41e343a94f
unknown <harshika0102@gmail.com>
2023-07-06 18:05:03 +0530

    added some code

--- solver.rb
+++ solver.rb
@@ -14,17 +14,6 @@ class Solver
     def reverse(word)
       word.reverse
     end
-  
-    def fizzbuzz(n)
-      if (n % 3).zero? && (n % 5).zero?
-        "fizzbuzz"
-      elsif (n % 3).zero?
-        "fizz"
-      elsif (n % 5).zero?
-        "buzz"
-      else
-        n.to_s
-      end
-    end
-  end
+  #fizzbuzz
+    
   
